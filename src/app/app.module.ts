ff096721b94253dc6c694a78c994d428d53b173c
Qasimshaheen <qasimshaheen@outlook.com>
2022-08-15 02:16:11 -0700

    Add RouterModules
    create application routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-
-import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employees.component';
-
-
+import { AppComponent } from "./app.component";
+import { ListEmployeesComponent } from "./employees/list-employees.component";
+import { CreateEmployeeComponent } from "./employees/create-employee.component";
+const appRoutes: Routes = [
+  { path: "list", component: ListEmployeesComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" },
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListEmployeesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
