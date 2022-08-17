321fd13a0fd1a1019e07509f591910ab0d381025
Qasimshaheen <qasimshaheen@outlook.com>
2022-08-17 10:04:33 -0700

    Custome Select Required Validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { BsDatepickerModule } from "ngx-bootstrap";
-
+import { SelectRequiredValidatorDirective } from "./shared/select-required-validator.directive";
 import { AppComponent } from "./app.component";
 import { ListEmployeesComponent } from "./employees/list-employees.component";
 import { CreateEmployeeComponent } from "./employees/create-employee.component";
@@ -13,7 +13,12 @@ const appRoutes: Routes = [
   { path: "", redirectTo: "/list", pathMatch: "full" },
 ];
 @NgModule({
-  declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent],
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent,
+    SelectRequiredValidatorDirective,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
